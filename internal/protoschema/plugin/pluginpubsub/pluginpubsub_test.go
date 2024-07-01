85dab084bdef1dcb355cf01bb3cbd818358a4811
Alfred Fuller <afuller@buf.build>
2024-07-01 15:48:32 -0700

    Migrate to buf v2 configs (#15)
    * Migrate to buf v2 configs
    
    * update deps
    
    * update

--- internal/protoschema/plugin/pluginpubsub/pluginpubsub_test.go
+++ internal/protoschema/plugin/pluginpubsub/pluginpubsub_test.go
@@ -48,7 +48,8 @@ func TestPubsubHandler(t *testing.T) {
 	require.NoError(t, err)
 	image, err := bufimage.NewImageForProto(protoImage)
 	require.NoError(t, err)
-	codeGeneratorRequest := bufimage.ImageToCodeGeneratorRequest(image, "", nil, false, false)
+	codeGeneratorRequest, err := bufimage.ImageToCodeGeneratorRequest(image, "", nil, false, false)
+	require.NoError(t, err)
 
 	request, err := protoencoding.NewWireMarshaler().Marshal(codeGeneratorRequest)
 	require.NoError(t, err)
